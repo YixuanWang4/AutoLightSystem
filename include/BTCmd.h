caa9adb11cafe2e77c00d4530ffb27c3b3647d2e
YixuanWang4 <wangyixuan200610@163.com>
2025-11-04 22:35:10 +0800

    Created the project framework

---
+++ include/BTCmd.h
@@ -0,0 +1,1 @@
+#include <BLE2902.h>
--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,17 @@
+/*
+  This is the main.cpp file for the project, which is the assignment of the Engineering Course.
+  It is written in C++11 standard, using Arduino framework and Google Coding Style.
+  The project will be running via the FreeRTOS operating system.
+  Maintainer: Yixuan Wang.
+*/
 
-
-// put function declarations here:
-int myFunction(int, int);
+#include <main.h>// Include the Arduino core library
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200); // Initialize serial communication at 115200 baud rate
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
